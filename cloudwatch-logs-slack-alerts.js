60e59591d71755ed7d4aebb5390bf190339e9cc7
janakaud <janakaud@gmail.com>
2020-05-06 11:42:34 +0530

    batched alerts from multiple log groups

---
+++ cloudwatch-logs-slack-alerts.js
@@ -0,0 +1,84 @@
+const AWS = require("aws-sdk");
+const logs = new AWS.CloudWatchLogs();
+
+
+const env = key => {
+	let val = process.env[key];
+	if (!val) {
+		throw new Error(`${key} unconfigured`);
+	}
+	return val;
+};
+
+
+const period = parseInt(env("POLL_PERIOD_MS"));
+if (!period) {
+	throw new Error(`Invalid poll period ${period}`);
+}
+const groups = env("LOG_GROUPS").split(" ");
+const filterPattern = env("LOG_PATTERN");
+const hook = env("SLACK_WEBHOOK_URL");
+const channel = env("SLACK_CHANNEL");
+const username = env("SLACK_USER");
+
+
+const axios = require("axios");
+const https = require("https");
+const httpsAgent = new https.Agent({keepAlive: true});
+
+const postMsg = text => {
+	return axios.post(hook, {
+		channel, username,
+		text,
+		icon_emoji: ":ghost:"
+	}, {httpsAgent});
+};
+
+
+exports.handler = async event => {
+	const startTime = Date.now() - period;
+
+	let timeStr = new Date(startTime).toTimeString();
+	timeStr = timeStr.substring(0, timeStr.indexOf(" "));
+	console.log(timeStr);
+
+    let checks = groups.map(async g => {
+		// if not namespaced, add default Lambda prefix
+		let logGroupName = g.indexOf("/") < 0 ? `/aws/lambda/${g}` : g;
+
+		let msg, partial = true;
+		try {
+			let data = await logs.filterLogEvents({
+				logGroupName,
+				filterPattern,
+				startTime,
+				limit: 100
+			}).promise();
+			msg = data.events.map(e => e.message.substring(0, 1000).trim()).join("\n\n");
+			partial = !!data.nextToken;
+		} catch (e) {
+			msg = `Failed to poll ${g}; ${e.message}`;
+		}
+		return msg.length > 0 ? `\`${g}${partial ? " [partial]" : ""}\`
+
+\`\`\`
+${msg}
+\`\`\`` : null;
+	});
+
+	// post gathered logs as one Slack msg
+    return await Promise.all(checks)
+		.then(msgs => {
+			let valid = msgs.filter(m => !!m);
+			if (valid.length > 0) {
+				postMsg(`\`${timeStr}\`
+
+${valid.join("\n\n")}`);
+			}
+		})
+		.catch(e => postMsg(`*FAULT* \`${timeStr}\`
+
+\`\`\`
+${e.message}
+\`\`\``));
+};
